91c264eadb341f9d2a6bbb47c978efa241fbc349
8DAMAGER8 <kuchkin.andrej@mail.ru>
2023-09-20 16:36:25 +0300

    add bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index", methods={"GET"})
+     */
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+}

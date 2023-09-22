f3fcaa3f3f57ce6e0fbe6a589d3541a47a722f28
8DAMAGER8 <kuchkin.andrej@mail.ru>
2023-09-22 14:22:49 +0300

    add bootstrap layout

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +13,12 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextareaType::class, ['label' => 'название'])
             ->add('description')
             ->add('quantity')
             ->add('price')
-            ->add('created')
-            ->add('updated')
+//            ->add('created')
+//            ->add('updated')
         ;
     }
 
